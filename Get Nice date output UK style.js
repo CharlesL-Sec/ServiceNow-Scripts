311ee359b1f3d615ecbadcf5c5b18ae6d23c21f5
CharlesL-Sec <173039445+CharlesL-Sec@users.noreply.github.com>
2026-02-19 09:24:02 +0000

    Implement UK date format output
    Added arrays for month and day names to format UK date.

--- Get Nice date output UK style.js
+++ Get Nice date output UK style.js
@@ -1,1 +1,41 @@
+// GlideScript to output date in format "19th March 2026"
+// Using Array method
+// Charles London
+// 19th February 2026
+var glideDateGB = new GlideDateTime();
+var monthNames = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ];
 
+var dayNames = [
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursdaay",
+	"Friday",
+	"Saturday",
+	"Sunday"
+
+];
+
+gs.log(dayNames[glideDateGB.getDayOfWeek()]);
+gs.log(monthNames[glideDateGB.getDayOfMonth()]);
+gs.log(glideDateGB.getYear());
+gs.log(glideDateGB.getDayOfMonthUTC());
+
+// glideDateGB.SetDisplayValueLang("11/19/2023 03:47:00", "short", "en-GB");
+
+
+
+// gs.info("en_GB date: " + glideDateGB.getDisplayValueLang("full"));
